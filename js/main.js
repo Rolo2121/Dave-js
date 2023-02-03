a118b3eb83ea63ab6ca96c0357e7841a1373e980
Jacob <jakebauer21@gmail.com>
2023-02-03 13:17:40 -0600

    added some js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,55 @@
-console.log('Hello Jake!');
-console.log(typeof 'Jaker');
-console.log(typeof 42);
-console.log(typeof true);
+// let customerIsBanned = false;
+// let soup = 'children soup';
+// let crakers = true;
+// let reply;
+// if (customerIsBanned) {
+// 	reply = 'No kids for you!';
+// } else if (soup && crakers) {
+// 	reply = `Here's your order of ${soup} & crakers!`;
+// } else if (soup) {
+// 	reply = `Here's your order of ${soup}`;
+// } else {
+// 	reply = `We out of kids to eat!`;
+// }
+// console.log(reply);
+
+// let testScore = 69;
+// let collegeStudent = true;
+// let grade;
+
+// if (testScore >= 90) {
+// 	grade = 'A';
+// } else if (testScore >= 80) {
+// 	grade = 'B';
+// } else if (testScore >= 70) {
+// 	grade = 'C';
+// } else if (testScore >= 69) {
+// 	grade = 'Noice!!!';
+// } else {
+// 	if (collegeStudent) {
+// 		grade = '69 Dudes!!!';
+// 	}
+// }
+// console.log(grade);
+
+if (playerOne === computer) {
+	// tie
+} else if (playerOne === 'rock') {
+	if (computer === 'paper') {
+		// computer wins
+	} else {
+		// player one wins
+	}
+} else if (playerOne === 'paper') {
+	if (computer === 'scissors') {
+		// computer wins
+	} else {
+		// player one wins
+	}
+} else {
+	if (computer === 'rock') {
+		// computer wins
+	} else {
+		// player one wins
+	}
+}
